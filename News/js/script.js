865318693b4ace87593f2a3eaa61462030c1295d
DonTofu <103603166+DonTofu@users.noreply.github.com>
2022-04-20 16:17:17 -0400

    Add files via upload

---
+++ News/js/script.js
@@ -0,0 +1,11 @@
+const $btnSignIn = document.querySelector('.btncolor1'),
+    $btnSignUp = document.querySelector('.btncolor'),
+    $signUp = document.querySelector('.sign-up'),
+    $signIn = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
---
+++ News/js/video.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("elvideo");
+var btn = document.getElementById("btnpause");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
